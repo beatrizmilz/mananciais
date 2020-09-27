32c4adfc793b7740ac7860a58e9f985f23baa37e
Beatriz Milz <beatriz.milz@hotmail.com>
2020-09-27 18:55:35 -0300

    add global variables, change R dependency

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -9,3 +9,23 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+
+globalVariables(c(
+  "volume_porcentagem_ar",
+  "volume_variacao_num",
+  "prec_dia",
+  "prec_mensal",
+  "prec_hist",
+  "nome",
+  "data",
+  "sistema",
+  "volume_porcentagem",
+  "volume_variacao",
+  "volume_operacional",
+  "pluviometria_dia",
+  "pluviometria_hist",
+  "data_original",
+  "data_valida",
+  "dia_busca"
+))
