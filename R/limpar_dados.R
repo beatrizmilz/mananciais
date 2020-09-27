439f2d6c1e725cc4a346b2a645ab01c203cec39f
Beatriz Milz <beatriz.milz@hotmail.com>
2020-09-27 10:44:38 -0300

    update and document function limpar_dados

--- R/limpar_dados.R
+++ R/limpar_dados.R
@@ -1,22 +1,36 @@
-#' Title
+#' Limpar dados.
 #'
-#' @param df
+#' Pode ser usada com o pipe ( %>% ) (veja exemplo abaixo).
+#' A limpeza consiste em: retirar colunas desnecessárias ou repetidas, altera o tipo de algumas variáveis.
 #'
-#' @return
-#' @export
+#' @param df Tibble a ser limpa. OPCIONAL. Caso for limpar dados que já foram baixados localmente, este argumento não é necessário.
+#' @param ano Numérico. OPCIONAL. Utilizado caso queira limpar dados baixados anteriormente.
+#' @param usar_baixados Lógico. Caso seja TRUE, utilizará os dados baixados anteriormente.
+#' @param diretorio_download Texto. OPCIONAL. Local onde os arquivos brutos estão salvos, caso `usar_baixados = TRUE`. Ex: `"data-raw/"`
+#' @param salvar_local Lógico. Caso seja TRUE, irá salvar o resultado localmente, como um arquivo `.rds`.
+#' @param diretorio_salvar Texto. OPCIONAL. Local onde salvar os arquivos limpos, caso `salvar_local = TRUE`. Ex: `"data-raw/data-clean/"`
 #'
-#' @examples
-limpar_dados <- function(ano,
+#' @return Retorna um tibble com os dados limpos.
+#'
+#' @examples  mananciais:::obter_dia("2020-09-26") %>% mananciais:::limpar_dados()
+#'
+#'
+limpar_dados <- function(df,
+                         ano,
+                         usar_baixados = FALSE,
                          diretorio_download = "data-raw/",
+                         salvar_local = FALSE,
                          diretorio_salvar = "data-raw/data-clean/") {
-  tabela_raw <-
-    readr::read_rds(glue::glue("{diretorio_download}mananciais_{ano}.rds"))
 
 
-  # colocar um if aqui, pois a limpeza depende. dados antigos não tem tantas colunas
+  if (usar_baixados == TRUE) {
+    df <-
+      readr::read_rds(glue::glue("{diretorio_download}mananciais_{ano}.rds"))
+  }
+
 
   dados_limpos <-
-    tabela_raw %>%
+    df %>%
     dplyr::mutate(
       volume_porcentagem = readr::parse_number(volume_porcentagem_ar, locale = readr::locale(decimal_mark = ",")),
       volume_variacao = volume_variacao_num,
@@ -33,10 +47,12 @@ limpar_dados <- function(ano,
       prec_dia:prec_hist
     )
 
-    dados_limpos %>%
+  if (salvar_local == TRUE) {
+  dados_limpos %>%
     # Exportar
     readr::write_rds(glue::glue("{diretorio_salvar}mananciais_{ano}.rds"),
                      compress = "xz")
+  }
 
   return(dados_limpos)
 }
--- R/obter_ano.R
+++ R/obter_ano.R
@@ -1,19 +1,18 @@
 #' Obtém dados de um ano.
 #'
 #' A tibble que retorna é a versão original obtida na API, ou seja, não foi limpa.
-#'
-#' Essa função demora alguns minutos para executar.
+#'#' Essa função demora alguns minutos para executar.
 #'
 #' @param ano  Numérico. Qual é o ano que deve ser feito o download. Ex: 2019.
 #' @param ano_atual  Lógico. Se quiser obter dados de anos anteriores, deve ser FALSE. Se quiser obter dados do ano atual, deve ser TRUE. Por padrão, é FALSE.
 #' @param download_local Lógico. Se for TRUE, uma cópia dos dados baixados será salvo localmente.
-#' @param diretorio_download Diretório onde o arquivo final deve ser salvo. Ex: "data-raw/"
+#' @param diretorio_download Diretório onde o arquivo final deve ser salvo. Ex: `"data-raw/"`
 #'
 #'
 #' @return Retorna um tibble com os dados do ano.
 #'
-#' @examples  mananciais:::obter_ano(2019, download_local = FALSE)
-#' @examples  mananciais:::obter_ano(2020, ano_atual = TRUE, download_local = FALSE)
+#' @examples  # mananciais:::obter_ano(2019, download_local = FALSE)
+#' @examples  # mananciais:::obter_ano(2020, ano_atual = TRUE, download_local = FALSE)
 obter_ano <-
   function(ano,
            ano_atual = FALSE,
